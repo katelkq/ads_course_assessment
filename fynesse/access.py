bee804a536dc155fd899b8135c14fc11a668ef9e
katelkq <kateliangkaiqing@outlook.com>
2023-11-18 20:07:22 +0000

    updates

--- fynesse/access.py
+++ fynesse/access.py
@@ -44,26 +44,24 @@ def create_connection(user, password, host, database, port=3306):
     pass
 
 
-def execute_queries(conn, queries, multi_line=False, fetch_rows=False):
+def execute_query(conn, query, multi_line=False, fetch_rows=True):
     """
     :param conn: handle to database connection
-    :param queries: a list of tuples, with each tuple potentially representing a multi-line query
+    :param queries: a string representing a single-line query, or a tuple of strings representing a multi-line query
     :param fetch_rows: do you want any rows to be returned?
     """
-    for query in queries:
-        
-        cursor = conn.cursor()
+    cursor = conn.cursor()
 
-        if multi_line:
-            count = cursor.execute('\n'.join(query))
-        else:
-            count = cursor.execute(query)
+    if multi_line:
+        count = cursor.execute('\n'.join(query))
+    else:
+        count = cursor.execute(query)
 
-        if fetch_rows:
-            print(f'{count} rows affected.')
-            print(tabulate(cursor.fetchall()))
-        else:
-            print(f'{count} rows affected.')
+    print(f'{count} rows affected.')
+
+    if fetch_rows:
+        print(tabulate(cursor.fetchall()))
+
     pass
 
 
@@ -92,13 +90,11 @@ def upload_pp_data(conn, path):
     for year in range(1995, 2022):
         print(f'Uploading data from year {year}...')
         for part in range(1, 3):
-            cursor = conn.cursor()
             filepath = os.path.join(path, f'pp-{year}-part{part}.csv')
             query = (f"LOAD DATA LOCAL INFILE '{filepath}' INTO TABLE `pp_data`",
                      "FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED by '\"'",
                      "LINES STARTING BY '' TERMINATED BY '\n';")
-            count = cursor.execute('\n'.join(query))
-            print(f'{count} rows affected.')
+            execute_query(conn, query=query, multi_line=True, fetch_rows=False)
     pass
 
 
@@ -115,26 +111,22 @@ def upload_postcode_data(conn, path):
     if path is None:
         path = '.'
 
-    cursor = conn.cursor()
     filepath = os.path.join(path, 'open_postcode_geo.csv')
     query = (f"LOAD DATA LOCAL INFILE '{filepath}' INTO TABLE `postcode_data`",
              "FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED by '\"'",
              "LINES STARTING BY '' TERMINATED BY '\n';")
-    count = cursor.execute('\n'.join(query))
-    print(f'{count} rows affected.')
+    execute_query(conn, query=query, multi_line=True, fetch_rows=False)
     pass
 
 
 def initialize_database(conn):
-    queries = ("SET SQL_MODE = \"NO_AUTO_VALUE_ON_ZERO\";",
-             "SET time_zone = \"+00:00\";",
-             "CREATE DATABASE IF NOT EXISTS `property_prices` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;",
-             "USE `property_prices`;")
+    queries = ["SET SQL_MODE = \"NO_AUTO_VALUE_ON_ZERO\";",
+               "SET time_zone = \"+00:00\";",
+               "CREATE DATABASE IF NOT EXISTS `property_prices` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;",
+               "USE `property_prices`;"]
     
     for query in queries:
-        cursor = conn.cursor()
-        count = cursor.execute(query)
-        print(f'{count} rows affected.')
+        execute_query(conn, query=query, multi_line=False, fetch_rows=False)
 
     print('Database initialized successfully.')
     pass
@@ -144,99 +136,96 @@ def initialize_table(conn, table):
     """
     Initializes the table schema and sets up the primary key.
     """
+
     match table:
         case 'pp_data':
-            # setting up the schema for table `pp_data`
-            count = conn.cursor().execute("USE `property_prices`;")
-            print(f'{count} rows affected.')
-
-            count = conn.cursor().execute("DROP TABLE IF EXISTS `pp_data`;")
-            print(f'{count} rows affected.')
-
-            cursor = conn.cursor()
-            query = ("CREATE TABLE IF NOT EXISTS `pp_data` (",
-                     "`transaction_unique_identifier` tinytext COLLATE utf8_bin NOT NULL,",
-                     "`price` int(10) unsigned NOT NULL,",
-                     "`date_of_transfer` date NOT NULL,",
-                     "`postcode` varchar(8) COLLATE utf8_bin NOT NULL,",
-                     "`property_type` varchar(1) COLLATE utf8_bin NOT NULL,",
-                     "`new_build_flag` varchar(1) COLLATE utf8_bin NOT NULL,",
-                     "`tenure_type` varchar(1) COLLATE utf8_bin NOT NULL,",
-                     "`primary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,",
-                     "`secondary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,",
-                     "`street` tinytext COLLATE utf8_bin NOT NULL,",
-                     "`locality` tinytext COLLATE utf8_bin NOT NULL,",
-                     "`town_city` tinytext COLLATE utf8_bin NOT NULL,",
-                     "`district` tinytext COLLATE utf8_bin NOT NULL,",
-                     "`county` tinytext COLLATE utf8_bin NOT NULL,",
-                     "`ppd_category_type` varchar(2) COLLATE utf8_bin NOT NULL,",
-                     "`record_status` varchar(2) COLLATE utf8_bin NOT NULL,",
-                     "`db_id` bigint(20) unsigned NOT NULL",
-                     ") DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;")
-            count = cursor.execute('\n'.join(query))
-            print(f'{count} rows affected.')
-
-            # adding the primary key for table `pp_data`
-            cursor = conn.cursor()
-            query = ("ALTER TABLE `pp_data`",
-                     "ADD PRIMARY KEY (`db_id`);")
-            count = cursor.execute('\n'.join(query))
-            print(f'{count} rows affected.')
-
-            cursor = conn.cursor()
-            query = ("ALTER TABLE `pp_data`",
-                     "MODIFY db_id bigint(20) unsigned NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;")
-            count = cursor.execute('\n'.join(query))
-            print(f'{count} rows affected.')
+            queries = ["USE `property_prices`;",
+                       "DROP TABLE IF EXISTS `pp_data`;",
+                       ("CREATE TABLE IF NOT EXISTS `pp_data` (",
+                        "`transaction_unique_identifier` tinytext COLLATE utf8_bin NOT NULL,",
+                        "`price` int(10) unsigned NOT NULL,",
+                        "`date_of_transfer` date NOT NULL,",
+                        "`postcode` varchar(8) COLLATE utf8_bin NOT NULL,",
+                        "`property_type` varchar(1) COLLATE utf8_bin NOT NULL,",
+                        "`new_build_flag` varchar(1) COLLATE utf8_bin NOT NULL,",
+                        "`tenure_type` varchar(1) COLLATE utf8_bin NOT NULL,",
+                        "`primary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,",
+                        "`secondary_addressable_object_name` tinytext COLLATE utf8_bin NOT NULL,",
+                        "`street` tinytext COLLATE utf8_bin NOT NULL,",
+                        "`locality` tinytext COLLATE utf8_bin NOT NULL,",
+                        "`town_city` tinytext COLLATE utf8_bin NOT NULL,",
+                        "`district` tinytext COLLATE utf8_bin NOT NULL,",
+                        "`county` tinytext COLLATE utf8_bin NOT NULL,",
+                        "`ppd_category_type` varchar(2) COLLATE utf8_bin NOT NULL,",
+                        "`record_status` varchar(2) COLLATE utf8_bin NOT NULL,",
+                        "`db_id` bigint(20) unsigned NOT NULL",
+                        ") DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;"),
+                       ("ALTER TABLE `pp_data`",
+                        "ADD PRIMARY KEY (`db_id`);"),
+                       ("ALTER TABLE `pp_data`",
+                        "MODIFY db_id bigint(20) unsigned NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;")]
 
         case 'postcode_data':
-            # setting up the schema for table `postcode_data`
-            count = conn.cursor().execute("USE `property_prices`;")
-            print(f'{count} rows affected.')
+            queries = ["USE `property_prices`;",
+                       "DROP TABLE IF EXISTS `postcode_data`;",
+                       ("CREATE TABLE IF NOT EXISTS `postcode_data` (",
+                        "`postcode` varchar(8) COLLATE utf8_bin NOT NULL,",
+                        "`status` enum('live','terminated') NOT NULL,",
+                        "`usertype` enum('small', 'large') NOT NULL,",
+                        "`easting` int unsigned,",
+                        "`northing` int unsigned,",
+                        "`positional_quality_indicator` int NOT NULL,",
+                        "`country` enum('England', 'Wales', 'Scotland', 'Northern Ireland', 'Channel Islands', 'Isle of Man') NOT NULL,",
+                        "`latitude` decimal(11,8) NOT NULL,",
+                        "`longitude` decimal(10,8) NOT NULL,",
+                        "`postcode_no_space` tinytext COLLATE utf8_bin NOT NULL,",
+                        "`postcode_fixed_width_seven` varchar(7) COLLATE utf8_bin NOT NULL,",
+                        "`postcode_fixed_width_eight` varchar(8) COLLATE utf8_bin NOT NULL,",
+                        "`postcode_area` varchar(2) COLLATE utf8_bin NOT NULL,",
+                        "`postcode_district` varchar(4) COLLATE utf8_bin NOT NULL,",
+                        "`postcode_sector` varchar(6) COLLATE utf8_bin NOT NULL,",
+                        "`outcode` varchar(4) COLLATE utf8_bin NOT NULL,",
+                        "`incode` varchar(3)  COLLATE utf8_bin NOT NULL,",
+                        "`db_id` bigint(20) unsigned NOT NULL",
+                        ") DEFAULT CHARSET=utf8 COLLATE=utf8_bin;"),
+                       ("ALTER TABLE `postcode_data`",
+                        "ADD PRIMARY KEY (`db_id`);"),
+                       ("ALTER TABLE `postcode_data`",
+                        "MODIFY `db_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;")]
 
-            count = conn.cursor().execute("DROP TABLE IF EXISTS `postcode_data`;")
-            print(f'{count} rows affected.')
-            
-            cursor = conn.cursor()
-            query = ("CREATE TABLE IF NOT EXISTS `postcode_data` (",
-                     "`postcode` varchar(8) COLLATE utf8_bin NOT NULL,",
-                     "`status` enum('live','terminated') NOT NULL,",
-                     "`usertype` enum('small', 'large') NOT NULL,",
-                     "`easting` int unsigned,",
-                     "`northing` int unsigned,",
-                     "`positional_quality_indicator` int NOT NULL,",
-                     "`country` enum('England', 'Wales', 'Scotland', 'Northern Ireland', 'Channel Islands', 'Isle of Man') NOT NULL,",
-                     "`latitude` decimal(11,8) NOT NULL,",
-                     "`longitude` decimal(10,8) NOT NULL,",
-                     "`postcode_no_space` tinytext COLLATE utf8_bin NOT NULL,",
-                     "`postcode_fixed_width_seven` varchar(7) COLLATE utf8_bin NOT NULL,",
-                     "`postcode_fixed_width_eight` varchar(8) COLLATE utf8_bin NOT NULL,",
-                     "`postcode_area` varchar(2) COLLATE utf8_bin NOT NULL,",
-                     "`postcode_district` varchar(4) COLLATE utf8_bin NOT NULL,",
-                     "`postcode_sector` varchar(6) COLLATE utf8_bin NOT NULL,",
-                     "`outcode` varchar(4) COLLATE utf8_bin NOT NULL,",
-                     "`incode` varchar(3)  COLLATE utf8_bin NOT NULL,",
-                     "`db_id` bigint(20) unsigned NOT NULL",
-                     ") DEFAULT CHARSET=utf8 COLLATE=utf8_bin;")
-            count = cursor.execute('\n'.join(query))
-            print(f'{count} rows affected.')
-
-            # adding the primary key
-            cursor = conn.cursor()
-            query = ("ALTER TABLE `postcode_data`",
-                     "ADD PRIMARY KEY (`db_id`);")
-            count = cursor.execute('\n'.join(query))
-            print(f'{count} rows affected.')
-
-            cursor = conn.cursor()
-            query = ("ALTER TABLE `postcode_data`",
-                     "MODIFY `db_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;")
-            count = cursor.execute('\n'.join(query))
-            print(f'{count} rows affected.')
+        case '':
+            queries = ["USE `property_prices`;",
+                       "DROP TABLE IF EXISTS `prices_coordinates_data`;",
+                       ("CREATE TABLE IF NOT EXISTS `prices_coordinates_data` (",
+                        "`price` int(10) unsigned NOT NULL,",
+                        "`date_of_transfer` date NOT NULL,",
+                        "`postcode` varchar(8) COLLATE utf8_bin NOT NULL,",
+                        "`property_type` varchar(1) COLLATE utf8_bin NOT NULL,",
+                        "`new_build_flag` varchar(1) COLLATE utf8_bin NOT NULL,",
+                        "`tenure_type` varchar(1) COLLATE utf8_bin NOT NULL,",
+                        "`locality` tinytext COLLATE utf8_bin NOT NULL,",
+                        "`town_city` tinytext COLLATE utf8_bin NOT NULL,",
+                        "`district` tinytext COLLATE utf8_bin NOT NULL,",
+                        "`county` tinytext COLLATE utf8_bin NOT NULL,",
+                        "`country` enum('England', 'Wales', 'Scotland', 'Northern Ireland', 'Channel Islands', 'Isle of Man') NOT NULL,",
+                        "`latitude` decimal(11,8) NOT NULL,",
+                        "`longitude` decimal(10,8) NOT NULL,",
+                        "`db_id` bigint(20) unsigned NOT NULL",
+                        ") DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;")
+            ]
 
         case _:
             raise ValueError
 
+    for (i, query) in enumerate(queries):
+        execute_query(conn, query=query, multi_line=(i>=2), fetch_rows=False)
+    pass
+
+
+def set_index(conn, table, column):
+    query = (f"CREATE INDEX {column}"
+             f"ON {table} ({column});")
+    execute_query(conn, query=query, multi_line=True, fetch_rows=False)
     pass
 
 
